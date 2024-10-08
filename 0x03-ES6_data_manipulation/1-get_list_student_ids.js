3d11e74e6fea3f108313c014088c6b637435b0e5
wakandee <koechallanhi@gmail.com>
2024-10-08 22:19:17 +0300

    map

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,16 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+* @author Allan Kipruto <https://github.com/wakandee>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
